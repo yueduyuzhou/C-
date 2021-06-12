16c40fcf034b975fe2af4a9f4a9a6b117747f16b
yueduyuzhou <2234669964@qq.com>
2021-06-12 12:59:08 +0800

    第一百八十九次上传

---
+++ 力扣_1217_玩筹码.c/力扣_1217_玩筹码.c/力扣_1217_玩筹码.c
@@ -0,0 +1,9 @@
+int minCostToMoveChips(int* position, int positionSize) {
+    int odd = 0, even = 0;
+    for (int i = 0; i < positionSize; i++)
+    {
+        if (position[i] % 2) { odd++; }
+        else { even++; }
+    }
+    return odd < even ? odd : even;
+}
