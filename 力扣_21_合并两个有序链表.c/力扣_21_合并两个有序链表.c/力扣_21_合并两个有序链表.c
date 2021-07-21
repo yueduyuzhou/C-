ef9bed52a265c3cabfbedc7b8a7c0f1aec36bdc1
yueduyuzhou <2234669964@qq.com>
2021-07-21 13:27:09 +0800

    第二百二十七次上传

---
+++ 力扣_21_合并两个有序链表.c/力扣_21_合并两个有序链表.c/力扣_21_合并两个有序链表.c
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2) {
+    if (l1 == NULL)
+        return l2;
+    if (l2 == NULL)
+        return l1;
+
+    struct ListNode* head;
+    struct ListNode* tail;
+
+    if (l1->val <= l2->val)
+    {
+        head = l1;
+        l1 = l1->next;
+    }
+    else
+    {
+        head = l2;
+        l2 = l2->next;
+    }
+    tail = head;
+
+    while (l1 && l2)
+    {
+        if (l1->val <= l2->val)
+        {
+            tail->next = l1;
+            l1 = l1->next;
+        }
+        else
+        {
+            tail->next = l2;
+            l2 = l2->next;
+        }
+        tail = tail->next;
+    }
+
+    if (!l1)
+        tail->next = l2;
+    if (!l2)
+        tail->next = l1;
+
+    return head;
+}
+
+
+/*****************************************************************************************/
